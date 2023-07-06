701ebf73b0498447cd13a50fcaaa836037f25b5d
Shami_Mourya <shamimourya9852@gmail.com>
2023-07-06 17:21:20 +0530

    user model

---
+++ server/models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter a name"],
+    },
+    avatar: {
+        public_id: String,
+        url: String,
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter a name"],
+        unique: [true, "Email already exists"],
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter a password"],
+        minlength: [6, "Password must be of 6 characters"],
+        select: false,
+    },
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+        }
+    ],
+    followers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+    following: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+    ]
+});
+
+module.exports = mongoose.model("User", userSchema);
