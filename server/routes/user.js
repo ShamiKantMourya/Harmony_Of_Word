30e4a657122762bc49da613e645d4c30597c36f9
Shami_Mourya <shamimourya9852@gmail.com>
2023-07-07 16:28:40 +0530

    user route

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/user");
-
+const { registerUser, loginUser, followUser } = require("../controllers/user");
+const {isAuthenticated} = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+
+router.route("/follow/:id").get(isAuthenticated, followUser);
 
 module.exports = router;
