a038a98c27055bf5a497db7575ac2dda2e4e5554
Shami_Mourya <shamimourya9852@gmail.com>
2023-07-07 21:36:34 +0530

    delete post

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { createPost, likeUnlikePost, deletePost, getFollowingPost } = require("../controllers/post");
+const { createPost, likeUnlikePost, deletePost, getFollowingPost, updateCaption } = require("../controllers/post");
 const { isAuthenticated } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.route("/createpost").post(isAuthenticated, createPost);
 router.route("/:id").get(isAuthenticated, likeUnlikePost);
+router.route("/:id").put(isAuthenticated, updateCaption);
 router.route("/:id").delete(isAuthenticated, deletePost);
 
 router.route("/followingPosts").get(isAuthenticated, getFollowingPost);
