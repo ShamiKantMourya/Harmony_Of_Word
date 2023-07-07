dcc7a96c268c7f8b01aebd5e7347616343c6f508
Shami_Mourya <shamimourya9852@gmail.com>
2023-07-07 12:31:25 +0530

    app

--- server/app.js
+++ server/app.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
 const app = express();
+
 
 if(process.env.NODE_ENV !== 'production'){
     require("dotenv").config({path: "server/config/.env"});
 }
+
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+//importing routes 
+const post = require("./routes/post");
+const user = require("./routes/user");
+
+//routes to use
+app.use("/post", post);
+app.use("/user", user);
+
 
 module.exports = app;
