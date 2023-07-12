060495417164872a6791da17e64a61f6fbbef9c1
Shami_Mourya <shamimourya9852@gmail.com>
2023-07-12 23:23:25 +0530

    reducer function

---
+++ frontend/src/Services/userService.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+export const loginUser = (email, password) => async(dispatch) => {
+    try {
+        dispatch({
+            type: "loginRequest",
+        });
+
+        const response = await axios.post("/user/login", {email, password},{
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        dispatch({
+            type: "loginSuccess",
+            payload: response.data.user,
+        });
+         
+    } catch (error) {
+        dispatch({
+            type: "loginFailure",
+            payload: error,
+        });
+    }
+};
+
+export const loadUser = () => async(dispatch) => {
+    try {
+        dispatch({
+            type: "loadUserRequest",
+        });
+
+        const response = await axios.get("/user/myProfile");
+        // console.log(response.data.user);
+        dispatch({
+            type: "loadUserSuccess",
+            payload: response.data.user,
+        });
+         
+    } catch (error) {
+        dispatch({
+            type: "loadUserFailure",
+            payload: error,
+        });
+    }
+}
