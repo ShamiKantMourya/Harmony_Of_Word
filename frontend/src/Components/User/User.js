b69f886d0dc6badee5e9eda38fb7a59effbf9fad
Shami_Mourya <shamimourya9852@gmail.com>
2023-07-12 23:22:13 +0530

    user component

---
+++ frontend/src/Components/User/User.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+import "./User.css";
+import { Typography } from '@mui/material';
+
+const User = ({ userId, name, location, avatar }) => {
+    return (
+        <Link to={`/user/${userId}`} className='user-card'>
+            <div className='user-box'>
+                <div className='user-img'>
+                    <img src={avatar} alt={name} />
+                </div>
+                <div className='name-loc'>
+                    <Typography className='loc'>{location}</Typography>
+                    <Typography className='name'>{name}</Typography>
+                </div>
+            </div>
+        </Link>
+    )
+}
+
+export default User;
