edbc0a7111c5c1b766437b3d2a1fd89163cba78b
Shami_Mourya <shamimourya9852@gmail.com>
2023-07-12 23:23:01 +0530

    code refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 import './App.css';
-import Header from "./Components/Header";
+import Header from "./Components/Header/Header";
+import Login from "./Components/Login/Login";
+import { loadUser } from "./Services/userService";
+import HomePage from "./Components/Home/HomePage";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadUser());
+  },[]);
+
+  const {isAuthenticated} = useSelector((state) => state.user);
   return (
     <Router>
-      <div className="App">
-        <Header />
-      </div>
+      {
+        isAuthenticated &&   <Header />
+      }
+      <Routes>
+        <Route path="/" element ={ isAuthenticated ? <HomePage /> : <Login />}/>
+      </Routes>
     </Router>
   );
 }
