55a6968febe1b9b487b7354cc26af5dd0e944969
Shami_Mourya <shamimourya9852@gmail.com>
2023-07-18 15:07:36 +0530

    get my post

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const {
     createPost,
     likeUnlikePost,
@@ -8,6 +9,8 @@ const {
     makeComments,
     deleteComment
 } = require("../controllers/post");
+
+const {getMyPosts} = require("../controllers/userProfile")
 const { isAuthenticated } = require("../middlewares/auth");
 
 const router = express.Router();
@@ -15,7 +18,11 @@ const router = express.Router();
 //Create, Update, Delete and Likeunlike Post
 router.route("/followingPosts").get(isAuthenticated, getFollowingPost);
 
+router.route("/my/posts").get(isAuthenticated, getMyPosts);
+
 router.route("/createpost").post(isAuthenticated, createPost);
+
+
 router.route("/:id").get(isAuthenticated, likeUnlikePost).put(isAuthenticated, updateCaption).delete(isAuthenticated, deletePost);
 
 
@@ -23,7 +30,6 @@ router.route("/:id").get(isAuthenticated, likeUnlikePost).put(isAuthenticated, u
 
 //Update, Add, Delete comment
 
-router.route("/comment/:id").put(isAuthenticated, makeComments);
-router.route("/comment/:id").delete(isAuthenticated, deleteComment);
+router.route("/comment/:id").put(isAuthenticated, makeComments).delete(isAuthenticated, deleteComment);
 
 module.exports = router;
