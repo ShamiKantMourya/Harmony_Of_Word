80e5ece37309df3ff03de0aaa5ff8718e3220dc8
Shami_Mourya <shamimourya9852@gmail.com>
2023-07-18 15:10:02 +0530

    cloudinary

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const app = require("./app");
 const {dataBase} = require("./config/database");
+const cloudinary = require("cloudinary");
 
 dataBase();
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
