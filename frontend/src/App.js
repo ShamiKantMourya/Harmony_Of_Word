dfab022e92cdedf62e8cad4c5f46fca65a7d9d27
Shami_Mourya <shamimourya9852@gmail.com>
2023-07-22 12:35:36 +0530

    update: route for register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,16 @@ import Header from "./Components/Header/Header";
 import Login from "./Components/Login/Login";
 import { loadUser } from "./Services/userService";
 import HomePage from "./Components/Home/HomePage";
+import UserAccount from "./Components/UserAccount/UserAccount";
+import CreatePost from "./Components/CreatePost/CreatePost";
+import SignUp from "./Components/SignUp/SignUp";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadUser());
-  },[]);
+  },[dispatch]);
 
   const {isAuthenticated} = useSelector((state) => state.user);
   return (
@@ -23,6 +26,9 @@ function App() {
       }
       <Routes>
         <Route path="/" element ={ isAuthenticated ? <HomePage /> : <Login />}/>
+        <Route path="/account" element ={ isAuthenticated ? <UserAccount /> : <Login />}/>
+        <Route path="/newpost" element ={ isAuthenticated ? <CreatePost /> : <Login />}/>
+        <Route path="/register" element ={ isAuthenticated ? <UserAccount /> : <SignUp />}/>
       </Routes>
     </Router>
   );
