7e7e15c2383f786db154a3257bfa07945b4d695a
Shami_Mourya <shamimourya9852@gmail.com>
2023-07-24 17:18:46 +0530

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,13 @@ import HomePage from "./Components/Home/HomePage";
 import UserAccount from "./Components/UserAccount/UserAccount";
 import CreatePost from "./Components/CreatePost/CreatePost";
 import SignUp from "./Components/SignUp/SignUp";
+import UpdateProfile from "./Components/UpdateProfile/UpdateProfile";
+import UpdatePassword from "./Components/UpdatePassword/UpdatePassword";
+import ForgetPassword from "./Components/Password/ForgetPassword";
+import ResetPassword from "./Components/Password/ResetPassword";
+import UserProfile from "./Components/UserProfile/UserProfile";
+import Search from "./Components/Search/Search";
+import NotFound from "./Components/NotFound/NotFound";
 
 function App() {
   const dispatch = useDispatch();
@@ -29,6 +36,13 @@ function App() {
         <Route path="/account" element ={ isAuthenticated ? <UserAccount /> : <Login />}/>
         <Route path="/newpost" element ={ isAuthenticated ? <CreatePost /> : <Login />}/>
         <Route path="/register" element ={ isAuthenticated ? <UserAccount /> : <SignUp />}/>
+        <Route path="/update/profile" element ={ isAuthenticated ? <UpdateProfile /> : <Login />}/>
+        <Route path="/update/password" element ={ isAuthenticated ? <UpdatePassword /> : <Login />}/>
+        <Route path="/forget/password" element ={ isAuthenticated ? <UpdatePassword /> :  <ForgetPassword />}/>
+        <Route path="/password/reset/:token" element ={ isAuthenticated ? <UpdatePassword /> :  < ResetPassword/>}/>
+        <Route path="/user/:id" element ={ isAuthenticated ? <UserProfile /> : <Login />}/>
+        <Route path="/search" element = { isAuthenticated ? <Search /> : <Login />} />
+        <Route path = "*" element = {<NotFound />} />
       </Routes>
     </Router>
   );
