1c91324acb5a134629b794bcd1bdebfb250e7c06
Shami_Mourya <shamimourya9852@gmail.com>
2023-07-24 22:40:00 +0530

    loading animation

--- frontend/src/Components/Loader/Loader.jsx
+++ frontend/src/Components/Loader/Loader.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Lottie from 'react-lottie';
+
+import LoaderAnimate from "../../Animations/Loader.json";
+import "./Loader.css";
 
 const Loader = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: LoaderAnimate,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
   return (
-    <div>
-        Loading
+    <div className='loader'>
+        <Lottie 
+                options={defaultOptions}
+                height={400}
+                width={400}
+                />
     </div>
   )
 }
