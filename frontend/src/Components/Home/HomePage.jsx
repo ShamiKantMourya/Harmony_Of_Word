07a43a032f37f404cdb6766e405097ea4e09f01e
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-04 20:57:54 +0530

    div:user profile area updated

--- frontend/src/Components/Home/HomePage.jsx
+++ frontend/src/Components/Home/HomePage.jsx
@@ -1,24 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Typography } from '@mui/material';
-import { useAlert } from 'react-alert';
-
+import { Typography } from "@mui/material";
+import { useAlert } from "react-alert";
 
 import "./HomePage.css";
-import User from '../User/User';
-import Post from '../Post/Post';
-import { getFollowingPost } from '../../Services/postService';
+import User from "../User/User";
+import Post from "../Post/Post";
+import { getFollowingPost } from "../../Services/postService";
 import { getAllUsers } from "../../Services/userService";
-import Loader from '../Loader/Loader';
+import Loader from "../Loader/Loader";
 // import CreatePost from '../CreatePost/CreatePost';
-
-
 
 const HomePage = () => {
   const dispatch = useDispatch();
 
-  const { loading, post, error } = useSelector((state) => state.postOfFollowing);
-  const { users, loading: userLoading } = useSelector((state) => state.allUsersData);
+  const { loading, post, error } = useSelector(
+    (state) => state.postOfFollowing
+  );
+  const { users, loading: userLoading } = useSelector(
+    (state) => state.allUsersData
+  );
   const { error: likeError, message } = useSelector((state) => state.like);
 
   const alert = useAlert();
@@ -41,17 +42,17 @@ const HomePage = () => {
       alert.success(message);
       dispatch({ type: "clearMessage" });
     }
-  }, [alert, likeError, error, message, dispatch])
+  }, [alert, likeError, error, message, dispatch]);
 
   return loading === true || userLoading === true ? (
     <Loader />
   ) : (
-    <div className='home-container'>
+    <div className="home-container">
       {/* Post Area */}
-      <div className='home-post-area'>
+      <div className="home-post-area">
         {/* <CreatePost /> */}
-        {
-          post && post.length > 0 ? post.map((posts) => (
+        {post && post.length > 0 ? (
+          post.map((posts) => (
             <Post
               key={posts._id}
               postId={posts._id}
@@ -64,28 +65,32 @@ const HomePage = () => {
               ownerName={posts.owner.name}
               ownerId={posts.owner._id}
             />
-          )) : <Typography variant="h6">No posts Available</Typography>
-        }
+          ))
+        ) : (
+          <Typography variant="h6">No posts Available</Typography>
+        )}
       </div>
 
       {/* //Friend List Area */}
-      <div className='home-friends-area'>
-        {
-          users && users.length > 0 ? users.map((user) =>
-          (
-            <User
-              key={user._id}
-              userId={user._id}
-              name={user.name}
-              location={user.location}
-              avatar={user.avatar.url}
-            />
-          )
-          ) : <Typography>No users found</Typography>
-        }
+      <div className="home-friends-area">
+        {users && users.length > 0 ? (
+          users.map((user) => (
+            <div className="user-profile-area">
+              <User
+                key={user._id}
+                userId={user._id}
+                name={user.name}
+                location={user.location}
+                avatar={user.avatar.url}
+              />
+            </div>
+          ))
+        ) : (
+          <Typography>No users found</Typography>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
