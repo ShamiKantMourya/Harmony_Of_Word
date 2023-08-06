937ed994f9194974764ccec0da47f4fde069258c
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-06 16:23:09 +0530

    added div toinput and button

--- frontend/src/Components/UpdatePassword/UpdatePassword.jsx
+++ frontend/src/Components/UpdatePassword/UpdatePassword.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { Typography, Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Typography } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-import {useAlert} from "react-alert";
+import { useAlert } from "react-alert";
 
 import "./UpdatePassword.css";
-import { updatePassword } from '../../Services/userService';
+import { updatePassword } from "../../Services/userService";
 
 const UpdatePassword = () => {
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
 
-  const {message,loading, error} = useSelector((state) => state.updateProfile);
+  const { message, loading, error } = useSelector(
+    (state) => state.updateProfile
+  );
   const dispatch = useDispatch();
   const alert = useAlert();
 
@@ -20,38 +22,48 @@ const UpdatePassword = () => {
   };
 
   useEffect(() => {
-if(error){
-  alert.error(error);
-  dispatch({type: "clearErrors"});
-}
-if(message){
-  alert.success(message);
-  dispatch({type: "clearMessage"})
-}
-  },[dispatch, alert, error, message])
+    if (error) {
+      alert.error(error);
+      dispatch({ type: "clearErrors" });
+    }
+    if (message) {
+      alert.success(message);
+      dispatch({ type: "clearMessage" });
+    }
+  }, [dispatch, alert, error, message]);
 
   return (
-    <div className='updatePassword'>
-      <Typography>Forget Password</Typography>
-      <form className='Update-password-form' onSubmit={handleForgetPassword}>
-        <input
-          type='password'
-          placeholder='Enter your old password'
-          required
-          value={oldPassword}
-          onChange={(event) => setOldPassword(event.target.value)}
-        />
-        <input
-          type='password'
-          required
-          placeholder='Enter your new password'
-          value={newPassword}
-          onChange={(event) => setNewPassword(event.target.value)}
-        />
-        <Button disabled = {loading} type='submit'>Change Password</Button>
-      </form>
+    <div className="updatePassword">
+      <div className="update-password-container">
+        <div className="update-password-text">
+          <Typography>Forget Password</Typography>
+        </div>
+        <form className="Update-password-form" onSubmit={handleForgetPassword}>
+          <input
+            type="password"
+            placeholder="Enter your old password"
+            required
+            value={oldPassword}
+            onChange={(event) => setOldPassword(event.target.value)}
+          />
+          <input
+            type="password"
+            required
+            placeholder="Enter your new password"
+            value={newPassword}
+            onChange={(event) => setNewPassword(event.target.value)}
+          />
+          <button
+            disabled={loading}
+            type="submit"
+            className="change-password-btn"
+          >
+            Change Password
+          </button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default UpdatePassword;
