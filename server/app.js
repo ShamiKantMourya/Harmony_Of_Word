2e2af18485452afde9f6d077f9552a47d0a6ddd9
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-07 16:17:05 +0530

    code format

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 
@@ -21,5 +22,10 @@ const user = require("./routes/user");
 app.use("/api/v1", post);
 app.use("/api/v1", user);
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+})
 
 module.exports = app;
