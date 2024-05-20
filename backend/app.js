a3d129f7a755e8aa0bea6a134aaeb131f0e83516
Shami_Mourya <shamimourya9852@gmail.com>
2024-05-20 15:53:56 +0530

    cors added

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const cors = require("cors");
+
+const { corsOptions } = require("./middlewares/cors.middleware");
 
 const app = express();
 
-
-if(process.env.NODE_ENV !== 'production'){
-    require("dotenv").config({path: "./.env"});
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: "./.env" });
 }
 
-
 //Middleware
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
-//importing routes 
+//importing routes
 const post = require("./routes/post");
 const user = require("./routes/user");
 
@@ -25,7 +27,7 @@ app.use("/api/v1", user);
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("*", (req, res) => {
-res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
-})
+  res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"));
+});
 
 module.exports = app;
---
+++ backend/middlewares/cors.middleware.js
@@ -0,0 +1,17 @@
+
+const allowedOrigins = ['http://localhost:3000','https://harmony-of-words-ca3m.onrender.com'];
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+module.exports = { corsOptions }
+
+// Use the cors middleware with the cors options for all routes
+
